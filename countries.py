3d88daa2b6117375fa5a1884bbf07e642e51c8e6
acabe10 <alejandrocabezab@gmail.com>
2019-03-07 17:06:01 +0100

    ejercicio1

--- countries.py
+++ countries.py
@@ -1,5 +1,13 @@
 import json
 with open("countries.json") as fichero:
-	datos=json.load(fichero)
+	doc=json.load(fichero)
 
-print(datos)
+pais=input("Dime un país: ").title()
+
+fronteras=[]
+for paises in doc:
+	if paises["name"]["common"] == pais:
+		if len(paises["borders"]):
+			print(paises["borders"])
+		else:
+			print("Este país no tiene fronteras, es una isla.")
